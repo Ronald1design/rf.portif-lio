df24ad6cb20621f502bd4782771705705d1181f5
Ronald1design <ronaldferrazsousa1@gmail.com>
2026-05-22 19:34:05 -0300

    Feat: update whatsapp number to +55 98 8124-1977

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -36,7 +36,7 @@ export default function Contact() {
       setStatusMsg({ type: 'success', text: 'Contato salvo! Redirecionando para o WhatsApp...' });
 
       // 2. Format WhatsApp Message (REPLACE PHONE NUMBER BELOW)
-      const whatsappNumber = '5511999999999'; 
+      const whatsappNumber = '559881241977'; 
       const text = `Olá Ronald!\n\nMeu nome é *${name}* (${email}).\n\nTenho interesse em um projeto de *${project}*.\n\n*Detalhes do projeto*:\n${message}`;
       const encodedText = encodeURIComponent(text);
       const whatsappUrl = `https://wa.me/${whatsappNumber}?text=${encodedText}`;
